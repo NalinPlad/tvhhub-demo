9006cb99f491936b1d1fb04d4e7b49624ecef519
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2022-10-29 15:38:05 -0700

    thing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Card from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  const projects = fetch(
+    "https://raw.githubusercontent.com/Whackalenso/tvhhub/main/frontend/data.json"
+  ).then((res) => res.json());
+  var cards = [];
+  for (let i = 0; i < projects.length; i++) {
+    cards.push(<Card>projects[i]</Card>);
+  }
+
+  return (
+    <div className="App">
+      <Grid>cards</Grid>
+    </div>
+  );
 }
 
 export default App;
